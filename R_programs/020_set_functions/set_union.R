33faec1c3e5a6ff58d55b23542ce7e0a0f126edd
Claudio Sartori <claudio.sartori@unibo.it>
2024-02-11 16:19:37 +0100

    Initial setup

--- R_programs/020_set_functions/set_union.R
+++
@@ -1,9 +0,0 @@
-set_union <- function(v1,v2){
-  # returns the union of two vectors;
-  # elements present in both input vectors are included only once in the result
-  # 
-  # simply obtained by combining the input vectors and returning
-  # the unique values
-  # requires: set_unique
-  return(set_unique(c(v1,v2)))
-} # end: set_union
--- R_programs/026_mergeSort/is_sorted.R
+++
@@ -1,9 +0,0 @@
-is.Sorted <- function(a){
-  if (length(a) == 1)
-    return(T)
-  sorted <- T
-  for(i in 2:length(a))
-    if(a[i] < a[i-1])
-      return(F)
-  return(T)
-}
