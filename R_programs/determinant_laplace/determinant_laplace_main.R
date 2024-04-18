8761a2e79ceb3eff8f734d60d2ead8cb54268a8d
Claudio Sartori <claudio.sartori@unibo.it>
2024-04-18 09:45:58 +0200

    determinant-laplace

---
+++ R_programs/determinant_laplace/determinant_laplace_main.R
@@ -0,0 +1,14 @@
+rm(list = ls())
+# setwd("~/Dropbox/Didattica/Informatics/0_R/R-class")
+source('determinant_laplace.R')
+n <- 12
+A <- matrix(as.double(sample(-1:1,n^2,T)), nrow = n)
+cat("Sparsity",sum(as.vector(A)==0)/length(A)*100,"\n")
+#print(A)
+ptm <- system.time(dL <- determinantLaplace(A))
+cat("Time determinant Laplace", ptm[3],"\n")
+ptm <- system.time(d <- det(A))
+cat("Time determinant standard library", ptm[3],"\n")
+# cat(
+#   "Value with Laplace ", dL,
+#     "\nValue with standard", d)
