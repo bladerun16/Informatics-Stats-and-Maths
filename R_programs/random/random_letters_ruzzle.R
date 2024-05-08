e7936ea9be0e1b07ac50e81a1d84d0d8b8638657
Claudio Sartori <claudio.sartori@unibo.it>
2024-05-08 16:32:32 +0200

    Update random_letters_ruzzle.R

--- R_programs/random/random_letters_ruzzle.R
+++ R_programs/random/random_letters_ruzzle.R
@@ -1,24 +1,37 @@
 rm(list = ls())
-#setwd("~/Dropbox/Didattica/Informatics/0_R/R-class")
-#setwd("M:/Sartori/Informatics-R-class")
-
-
-ruzzle <- sample(letters,16,replace = T)
+# first trial: naive solution
+ruzzle <- sample(LETTERS,16,replace = T)
+print(ruzzle)
+# rearrange into a square
 ruzzle <- matrix(ruzzle, nrow = 4)
 # each letter has the same probability
 print(ruzzle)
 
-# read the file of letter probabilities
-# check that the double have . and not ,
+# Generate a Ruzzle schema for the 
+# english language
+# - download the file of the letter frequencies
+#   for the appropriate language
+# - inspect visually the file and see if we
+#   need to set the separator and the header, 
+#   if necessary reload the file into dataframe 
+#   let_prob, names (Letter, Prob)
+# - generate a matrix with the appropriate size
+#   using "sample" with parameters 
+#   - x=Letter, 
+#   - size = desired size
+#   - replace = True,
+#   - prob=Prob
+#   
+
 letter_freq <- read.csv(  "letter_freq.csv"
                         , header = FALSE
                         , sep = ";")
-normalize <- sum(letter_freq$V2)
-freq <- letter_freq$V2 / normalize
-ruzzle <- matrix(  sample(  letter_freq$V1
+names(letter_freq) <- c("Letter", "Freq")
+letter_freq$Freq <- letter_freq$Freq / sum(letter_freq$Freq)
+ruzzle <- matrix(  sample(  letter_freq$Letter
                 , 16
                 , replace = TRUE
-                , prob = freq
+                , prob = letter_freq$Freq
                 )
        , nrow = 4)
 
