6228063754e65d579f4230455b564590ed6155c6
Claudio Sartori <claudio.sartori@unibo.it>
2024-05-09 16:08:18 +0200

    Update ellipse.R

--- R_programs/random/ellipse.R
+++ R_programs/random/ellipse.R
@@ -6,7 +6,7 @@ is_inside_ellipse <- function(x, y, a, b) {
 }
 
 # Define the Monte Carlo simulation function
-monte_carlo_area <- function(n_simulations, a, b) {
+monte_carlo_ellipse_area <- function(n_simulations, a, b) {
   count_inside <- 0
   
   for (i in 1:n_simulations) {
@@ -26,7 +26,20 @@ monte_carlo_area <- function(n_simulations, a, b) {
   estimated_area <- total_area * area_ratio
   
   return(estimated_area)
-}
+} # monte_carlo_ellipse_area - end
+
+# Define the Monte Carlo simulation function
+monte_carlo_ellipse_area_vec <- function(n_simulations, a, b) {
+  x <- runif(n_simulations, -a, a)
+  y <- runif(n_simulations, -b, b)
+  e <- ((x^2) / (a^2)) + ((y^2) / (b^2)) <= 1
+  # Calculate the area of the ellipse based on the proportion of points inside the ellipse
+  area_ratio <- length(e) / n_simulations
+  total_area <- 4 * a * b
+  estimated_area <- total_area * area_ratio
+    
+  return(estimated_area)
+} # monte_carlo_ellipse_area - end
 
 # Define the parameters of the ellipse
 a <- 3  # semi-major axis
@@ -34,5 +47,5 @@ b <- 2  # semi-minor axis
 n_simulations <- 1000000  # number of Monte Carlo simulations
 
 # Compute the estimated area of the ellipse
-estimated_area <- monte_carlo_area(n_simulations, a, b)
+estimated_area <- monte_carlo_ellipse_area_vec(n_simulations, a, b)
 cat("Estimated area of the ellipse:", estimated_area, "\n")
