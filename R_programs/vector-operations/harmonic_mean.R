8e951729862c59cc9423169c8e03239b83d7ae5f
Claudio Sartori <claudio.sartori@unibo.it>
2025-03-04 09:59:14 +0100

    vector-operations

---
+++ R_programs/vector-operations/harmonic_mean.R
@@ -0,0 +1,26 @@
+### GOAL
+# compute the harmonic mean of values of a vector
+# (harmonic mean = reciprocal (1/x or x^-1) of mean of reciprocals)
+
+### PSEUDOCODE
+# initialize sum to 0
+# initialize i to 1
+# while i is less or equal to length of vector
+#   increment sum by reciprocal of i-th element of vector
+#   increment i by 1
+# compute hm as length of vector divided by sum
+# print hm
+
+### CODE
+
+a <- c( 2, 5, 7, 4, 1 )
+
+sum <- 0
+i <- 1
+while ( i <= length(a) ) {
+  sum <- sum + ( 1 / a[i] )
+  i <- i + 1
+}
+hm <- length(a) / sum
+cat( "The harmonic mean is", hm )
+
