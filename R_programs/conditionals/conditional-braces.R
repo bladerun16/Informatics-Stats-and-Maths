70df524b69177ea6759c50b43249b3db74121d81
Claudio Sartori <claudio.sartori@unibo.it>
2026-02-06 22:37:49 +0100

    reset

--- R_programs/conditionals/conditional-braces.R
+++
@@ -1,8 +0,0 @@
-a <- 6
-b <- 7
-if (a < b) {
-  print("a is smaller than b")
-} else
-{
-  print("a is not smaller than b")
-}
--- R_programs/rref/randomMatrix.R
+++
@@ -1,8 +0,0 @@
-# Set the size of the matrix
-n <- 5  # Change this to any size you want
-
-# Generate the random 0/1 matrix
-random_matrix <- matrix(sample(c(0, 1), n * n, replace = TRUE), nrow = n, ncol = n)
-
-# Print the matrix
-print(random_matrix)
